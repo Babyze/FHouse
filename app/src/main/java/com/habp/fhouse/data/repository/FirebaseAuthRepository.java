048150fc72110f9c772d5c168d47c53c24b511c3
Dang Huynh Anh <danghuynhanh@gmail.com>
2021-03-15 18:23:01 +0700

    Update Callback

--- app/src/main/java/com/habp/fhouse/data/repository/FirebaseAuthRepository.java
+++ app/src/main/java/com/habp/fhouse/data/repository/FirebaseAuthRepository.java
@@ -3,10 +3,10 @@ package com.habp.fhouse.data.repository;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.habp.fhouse.data.datasource.FirebaseAuthRemote;
-import com.habp.fhouse.util.CallBack;
 
 public class FirebaseAuthRepository {
     private final FirebaseAuthRemote firebaseAuthRemote;
+    private boolean[] result = new boolean[1];
 
     public FirebaseAuthRepository(FirebaseAuth firebaseAuth, FirebaseFirestore firebaseFirestore) {
         firebaseAuthRemote = new FirebaseAuthRemote(firebaseAuth, firebaseFirestore);
@@ -21,19 +21,16 @@ public class FirebaseAuthRepository {
     }
 
     public boolean signIn(String email, String password) {
-        final boolean[] result = new boolean[1];
         firebaseAuthRemote.signIn(email, password, isSuccess -> result[0] = isSuccess);
         return result[0];
     }
 
     public boolean signUp(String email, String password, String name, String phone) {
-        final boolean[] result = new boolean[1];
         firebaseAuthRemote.signUp(email, password, name, phone, isSuccess -> result[0] = isSuccess);
         return result[0];
     }
 
     public boolean resetPassword(String email) {
-        final boolean[] result = new boolean[1];
         firebaseAuthRemote.resetPassword(email, isSuccess -> result[0] = isSuccess);
         return result[0];
     }
