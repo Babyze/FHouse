498cd7d93ecdaee8030a9142d201932cd9441434
babyz <babyz@ALEXANDER>
2021-03-17 15:12:40 +0700

    Update Contraints Database and Profile

--- app/src/main/java/com/habp/fhouse/util/DatabaseConstraints.java
+++ app/src/main/java/com/habp/fhouse/util/DatabaseConstraints.java
@@ -13,5 +13,7 @@ public class DatabaseConstraints {
     public static final String BED_ID_KEY_NAME = "bedId";
     public static final String ARTICLE_TIME_KEY_NAME = "createAt";
     public static final String ARTICLE_ID_KEY_NAME = "articleId";
-    public static final String IMAGE_PATH = "images/boarding/";
+    public static final String BOARDING_IMAGE_PATH = "images/boarding/";
+    public static final String PROFILE_IMAGE_PATH = "images/profile";
+    public static final String PROFILE_DEFAULT_IMAGE_PATH = PROFILE_IMAGE_PATH + "/" + "avatar.jpg";
 }
