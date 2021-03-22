d40c0d9d96da9cf5b7348def6633a714feddd70e
Dang Huynh Anh <danghuynhanh@gmail.com>
2021-03-23 01:40:05 +0700

    Fix bug

--- app/src/main/java/com/habp/fhouse/ui/home/HomePresenter.java
+++ app/src/main/java/com/habp/fhouse/ui/home/HomePresenter.java
@@ -7,8 +7,12 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.habp.fhouse.data.datasource.ArticleFirestoreRepository;
 import com.habp.fhouse.data.datasource.UserFirestoreRepository;
+import com.habp.fhouse.data.model.Article;
 import com.habp.fhouse.data.model.ArticleSnap;
 import com.habp.fhouse.util.CallBack;
+
+import java.util.Collections;
+import java.util.Comparator;
 
 public class HomePresenter implements HomeContract.Presenter {
     private ArticleFirestoreRepository articleFirestoreRepository;
@@ -38,6 +42,12 @@ public class HomePresenter implements HomeContract.Presenter {
             articleFirestoreRepository.getNewestArticleList(snap, articleSnap -> callBack.onSuccessListener(articleSnap));
         } else {
             articleFirestoreRepository.getNewestArticleList(articleSnap -> {
+                Collections.sort(articleSnap.getArticleList(), new Comparator<Article>() {
+                    @Override
+                    public int compare(Article o1, Article o2) {
+                        return o2.getCreateAt().compareTo(o1.getCreateAt());
+                    }
+                });
                 callBack.onSuccessListener(articleSnap);
             });
         }
